51205aa1ed12f9e16ddc2d0e7bd8e82a47abf8f5
qmdl546 <hyeonu4945@gmail.com>
2022-02-17 01:41:21 +0900

    FBV로 블로그 포스트목록 페이지만들기

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def index(request):
+    posts = Post.objects.all().order_by('-pk') #.order_by('-pk') 최신이 맨위로 정렬
+
+    return render(
+        request,
+        'blog/index.html',
+        {
+            'posts': posts,
+        }
+    )
